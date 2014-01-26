ebe72ec9bbcadf47505590c125b26cbbd6db2ced
jakob <jakob@videoplaza.com>
2014-01-26 04:08:01 +0100

    Reset server ip.

--- client/app/scripts/config.js
+++ client/app/scripts/config.js
@@ -1,5 +1,7 @@
 define([], function() {
 	return {
-		server:"http://127.0.0.1:3000"
+		server: "http://130.237.13.89:3000"
+		//server:"http://10.0.2.2:3000"
+		//server:"http://127.0.0.1:3000"
 	};
 });
